970bdeba6fdab709291213ef4f56fd78d4369e57
root <root@ip-172-31-11-148.ap-south-1.compute.internal>
2021-04-19 06:03:40 +0000

    f1

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 Class Simple{
   public static void main(string args[]){
  
-    system.out.println(" Welcome to Miracle RKIT SOFTWARE ");
+    system.out.println(" Welcome to Miracle RKIT SOFTWARE pvt ltd ");
   }
 }
